b3efd3cf1a36eec28d4120555749d50ec34e11be
luishsilva <luizhrick@gmail.com>
2024-11-29 07:59:11 -0700

    Associative arrays

--- index.php
+++ index.php
@@ -20,17 +20,45 @@
 
     <?php 
         $books = [
-            "The Bible",
-            "Hidden Potential",
-            "Sabbath School Lesson"
+            [
+                'name' => 'The Bible',
+                'author' => 'Jesus',
+                'purchaseUrl' => 'http://exempple.com'
+            ],
+            [
+                'name' => 'Hidden Potential',
+                'author' => 'Adam Grant',
+                'purchaseUrl' => 'http://exempple.com'
+            ],
+            [
+                'name' => 'Sabbath School Lesson',
+                'author' => 'John Doe',
+                'purchaseUrl' => 'http://exempple.com'
+            ],       
+            [
+                'name' => 'Smart Kids',
+                'author' => 'Mary Doe',
+                'purchaseUrl' => 'http://exempple.com'
+            ]
         ];
     ?>
 
     <h2>Arrays</h2>
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?=$book?></li>
+            <li>
+                <a href="<?=$book['name']?>">
+                    <?=$book['name']?>
+                </a>
+                <span>
+                    by:
+                    <?=$book['author']?>
+                </span>
+            </li>
         <?php endforeach; ?>
     </ul>
+
+    <h2>Accessing arrays indexes</h2>
+    <?=$books[0]['name'];?>
 </body>
 </html>
