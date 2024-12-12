4f1fe0be8934c38b911f3fa887e3d549e13d1492
luishsilva <luizhrick@gmail.com>
2024-12-11 22:47:38 -0700

    Connect with pdo and show first result from the database

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require('functions.php');
 // require('router.php');
 
 // connection to Mysql database.
-class Person {
-    public $name;
-    public $age;
 
-    public function breathe() {
-        echo $this->name . ' is breathing';
-    }
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    echo "<li>" .$post['title']."</li>";
 }
-
-$person = new Person();
-
-$person->name = 'John Doe';
-$person->age = 25;
-
-$person->breathe();
