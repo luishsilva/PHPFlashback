6bcf4cceccd15be8736d048d87e684407be0a478
luishsilva <luizhrick@gmail.com>
2024-12-16 23:51:14 -0700

    Add create note form

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,3 +1,9 @@
 <?php
 $heading = 'Create Note';
+
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) 
+{ 
+    dd($_POST);
+}
+
 require('views/create-notes.view.php');
--- views/partials/head.php
+++ views/partials/head.php
@@ -3,7 +3,7 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
 <body class="h-full">
     <div class="min-h-full">
