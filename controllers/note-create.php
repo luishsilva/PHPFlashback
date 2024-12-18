826666016a99ccad59670abfe7a84dac66b3f248
luishsilva <luizhrick@gmail.com>
2024-12-17 23:42:20 -0700

    Add validation to a post request when submiting a new note

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,10 +7,24 @@ $heading = 'Create Note';
 
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) 
 { 
-    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-        'body'=> $_POST['body'],
-        'user_id'=> 1
-    ] );
+    $errors = [];
+
+    $body = trim($_POST['body']);
+
+    if(strlen($body) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($body) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body'=> $body,
+            'user_id'=> 1
+        ]);
+    }
 }
 
 require('views/create-notes.view.php');
--- views/create-notes.view.php
+++ views/create-notes.view.php
@@ -12,7 +12,16 @@
           <div class="col-span-full">
             <label for="about" class="block text-sm/6 font-medium text-gray-900">Description</label>
             <div class="mt-2">
-              <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              <textarea 
+                name="body" 
+                id="body" 
+                rows="3" 
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                placeholder="Here is a idea for a note..."
+                ><?= $_POST['body'] ?? '' ?></textarea>
+              <?php if (isset($errors['body'])) : ?>
+                <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+              <?php endif; ?>
             </div>
           </div>
         </div>
