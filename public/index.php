8a87dede691d298833e5ef204614ea0dad7d5190
luishsilva <luizhrick@gmail.com>
2024-12-20 16:03:24 -0700

    Move functions and router to Core directory, also fixes the errors related to paths

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 const BASE_PATH = __DIR__ .'/../'; 
 
-require(BASE_PATH.'functions.php');
+require(BASE_PATH.'Core/functions.php');
 
 // autoload Database class because it's being instantiated in the controller ($db = new Database($config['database']);)
 spl_autoload_register(function ($class) {
     require base_path( "Core/{$class}.php");
 });
 
-require(base_path('router.php'));
+require(base_path('Core/router.php'));
 
