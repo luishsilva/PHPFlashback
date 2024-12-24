86aa50c44e7457da543e743c657d8894cdcc7508
luishsilva <luizhrick@gmail.com>
2024-12-23 23:34:47 -0700

    Refactor router to a class file also refactor routes file

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -13,6 +13,9 @@
         <?= htmlspecialchars($note['body']) ?>
       </p>
       <form method="POST" class="mt-6">
+        <!-- trick form to send a "DELETE METHOD" -->
+        <input type="hidden" name="_method" value="DELETE">
+        
         <input type="hidden" name="id" value="<?= $note['id']; ?>"/>
         <button class="text-sm text-red-500">Delete</button>
       </form>
