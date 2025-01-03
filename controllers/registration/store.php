0cc65c15bfedfa9a2db093127f25ede2c4ae6c18
luishsilva <luizhrick@gmail.com>
2025-01-02 23:41:01 -0700

    Add registration view and controller

---
+++ controllers/registration/store.php
@@ -0,0 +1,45 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+use Core\App;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if(!Validator::email($_POST['email'])) {
+    $errors['email'] = 'Please provide a valid address';
+}
+
+if(!Validator::string($_POST['password'], 7, 255)) {
+    $errors['password'] = 'Please provide a password with at least 7 characters';
+}
+
+if (!empty($errors)) { 
+    return view('registration/create.view.php', [
+        'errors'=> $errors
+    ]);
+}
+
+$db = App::resolve((Database::class));
+
+$user = $db->query('SELECT * FROM users WHERE email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    header('location: /');
+    exit();
+} else {
+    $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+        'email'=> $email,
+        'password' => $password
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}
