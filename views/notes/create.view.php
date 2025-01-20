69be91638ab22af25a4a386932be10046cd38e5f
luishsilva <luizhrick@gmail.com>
2025-01-19 23:55:24 -0700

    Add old form login email session function to return the email if something went wrong

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -18,7 +18,7 @@
                 rows="3" 
                 class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 placeholder="Here is a idea for a note..."
-                ><?= $_POST['body'] ?? '' ?></textarea>
+                ><?= old('email') ?? '' ?></textarea>
               <?php if (isset($errors['body'])) : ?>
                 <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
               <?php endif; ?>
